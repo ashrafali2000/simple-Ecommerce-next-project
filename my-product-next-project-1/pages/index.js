903e71cbe9a14cae9d830c5983660e1d4bdc55cd
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-08-07 11:28:21 +0500

    create card products

--- my-product-next-project-1/pages/index.js
+++ my-product-next-project-1/pages/index.js
@@ -1,8 +1,15 @@
-
+import { useState, useEffect } from "react"
+import Products from "./products/Products";
 export default function Home() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("/api/products")
+      .then((res) => res.json())
+      .then((p) => setProducts(p.products));
+  }, []);
   return (
    <div>
-     
+     {products.map(product => <Products key={product.id} img = {product.images[0]} titel = {product.title} stock = {product.stock} price={product.price} /> )}
    </div>
   )
 }
--- my-product-next-project-1/services/products.js
+++ my-product-next-project-1/services/products.js
@@ -1,5 +1,6 @@
 import fs from "fs";
 import path from "path";
+
 const filePath = path.join(process.cwd(), "database","products.json" );
 
 export function getAllProducs() {
