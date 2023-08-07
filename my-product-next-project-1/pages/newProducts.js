2693d4d00aa58476185025c37386dd3c2a07db95
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-08-07 14:42:23 +0500

    css fro new item

--- my-product-next-project-1/pages/newProducts.js
+++ my-product-next-project-1/pages/newProducts.js
@@ -8,6 +8,12 @@ function NewProduct() {
   const titleRef = useRef();
   const priceRef = useRef();
   const descriptionRef = useRef();
+  const discountPercentagenRef = useRef();
+  const ratingRef = useRef();
+  const stockRef = useRef();
+  const brandRef = useRef();
+  const categoryRef = useRef();
+  const imagesRef = useRef();
 
   function addProductHandler(event) {
     event.preventDefault();
@@ -15,12 +21,18 @@ function NewProduct() {
     const title = titleRef.current.value;
     const price = priceRef.current.value;
     const description = descriptionRef.current.value;
+    const discountPercentage = discountPercentagenRef.current.value;
+    const rating = ratingRef.current.value;
+    const stock = stockRef.current.value;
+    const brand = brandRef.current.value;
+    const category = categoryRef.current.value;
+    const images = imagesRef.current.value;
 
     if (!title || title.trim() === "" || !price || price.trim() === "") {
       setIsInvalid(true);
       return;
     }
-    const newProduct = JSON.stringify({ title, price, description });
+    const newProduct = JSON.stringify({title,description,price, discountPercentage, rating,stock,brand,category,images });
     fetch("/api/products/", {
       method: "POST",
       body: newProduct,
@@ -32,6 +44,7 @@ function NewProduct() {
       .then((json) => console.log(json));
   }
 
+
   return (
     <form className={classes.form} onSubmit={addProductHandler}>
       <div className={classes.row}>
@@ -41,7 +54,31 @@ function NewProduct() {
         </div>
         <div className={classes.control}>
           <label htmlFor="price">Price</label>
-          <input type="number" id="number" ref={priceRef} />
+          <input type="number" id="price" ref={priceRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="discountPercentage">discountPercentage</label>
+          <input type="number" id="discountPercentage" ref={discountPercentagenRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="rating">rating</label>
+          <input type="number" id="rating" ref={ratingRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="stock">stock</label>
+          <input type="number" id="stock" ref={stockRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="brand">brand</label>
+          <input type="text" id="brand" ref={brandRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="category">category</label>
+          <input type="text" id="category" ref={categoryRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="images">images</label>
+          <input type="number" id="images" ref={imagesRef} />
         </div>
       </div>
       <div className={classes.control}>
