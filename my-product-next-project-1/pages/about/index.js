6f34d09d21cbffb8181d6ac4b7b5bfec5662c5d0
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-08-07 16:59:01 +0500

    delete img

--- my-product-next-project-1/pages/about/index.js
+++ my-product-next-project-1/pages/about/index.js
@@ -6,16 +6,16 @@ const links = [
   ]
   const stats = [
     { name: 'Offices worldwide', value: '12' },
-    { name: 'Full-time colleagues', value: '300+' },
+    { name: 'Full-time colleagues', value: '400+' },
     { name: 'Hours per week', value: '40' },
     { name: 'Paid time off', value: 'Unlimited' },
   ]
   
   export default function Example() {
     return (
-      <div className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
+      <div className="relative isolate overflow-hidden bg-gray-500 py-24 sm:py-32">
         <img
-          src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
+          src="https://t4.ftcdn.net/jpg/05/64/60/45/360_F_564604544_KNKxKUoQdEu8yYSQlrygkzdKRnuR1zSp.jpg"
           alt=""
           className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
         />
@@ -45,7 +45,7 @@ const links = [
         </div>
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="mx-auto max-w-2xl lg:mx-0">
-            <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">Work with us</h2>
+            <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">Stay with us</h2>
             <p className="mt-6 text-lg leading-8 text-gray-300">
               Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
               fugiat veniam occaecat fugiat aliqua.
