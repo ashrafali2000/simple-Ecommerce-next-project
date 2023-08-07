28f9b4dbc19a600e79362b5028582c62e82e68a0
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-08-07 10:20:01 +0500

    write home about contact names

--- my-product-next-project-1/pages/_app.js
+++ my-product-next-project-1/pages/_app.js
@@ -5,10 +5,10 @@ import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 
 
 const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
+  { name: 'Home', href: '#', current: true },
+  { name: 'About', href: '#', current: false },
+  { name: 'Products', href: '#', current: false },
+  { name: 'Contact', href: '#', current: false },
 ]
 
 function classNames(...classes) {
