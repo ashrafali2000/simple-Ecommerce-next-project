578c71b55696132e5a46a9a4e1cf0c03e78f146b
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-08-07 09:54:00 +0500

    add contect of tailwind css

--- my-product-next-project-1/pages/_app.js
+++ my-product-next-project-1/pages/_app.js
@@ -1,5 +1,15 @@
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  <div>
+{/* Nav-bar */}
+
+
+
+
+
+    return <Component {...pageProps} />
+
+  </div>
 }
