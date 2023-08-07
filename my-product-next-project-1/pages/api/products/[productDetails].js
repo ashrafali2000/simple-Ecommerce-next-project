cdd238e65e06ef68a0207becbd2f1a93c92c9a7d
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-08-07 14:23:21 +0500

    get products by id

--- my-product-next-project-1/pages/api/products/[productDetails].js
+++ my-product-next-project-1/pages/api/products/[productDetails].js
@@ -1,6 +1,10 @@
+import { getSingleProducsById } from "@/services/products";
 export default function handler(req, res) {
     if(req.method === "GET") {
-        console.log(req.query);
-        
+        const {productDetails} = req.query;
+        const product = getSingleProducsById(Number(productDetails));
+    return res.status(200).json(product);
     }
+    return res.status(404).json({ message: "Not found" });
 }
+
