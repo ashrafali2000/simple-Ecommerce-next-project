45bc7a70dc1b60bc29f6e336fbf1e9a225320477
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-08-08 17:28:29 +0500

    create comment function

--- my-product-next-project-1/pages/api/products/index.js
+++ my-product-next-project-1/pages/api/products/index.js
@@ -1,17 +1,20 @@
-import { getAllProducs, addProduct } from "@/services/products"
+import { getAllProducs, addProduct,getCommentById } from "@/services/products"
 
 export default function handler(req, res) {
     if(req.method === "GET") {
         const product = getAllProducs();
         res.status(200).json(product);
     }
+    // if (req.method === "POST") {
+    //     const {title,description,price, discountPercentage, rating,stock,brand,category,images } = req.body;
+    //     const updatedProducts = addProduct(title,description,price, discountPercentage, rating,stock,brand,category,images);
+    //     return res.status(201).json(updatedProducts);
+    //   }
     if (req.method === "POST") {
-        const {title,description,price, discountPercentage, rating,stock,brand,category,images } = req.body;
-        const updatedProducts = addProduct(title,description,price, discountPercentage, rating,stock,brand,category,images);
+        const {myKey,comment } = req.body;
+        const updatedProducts = getCommentById(myKey,comment);
+        // console.log(id, comment);
         return res.status(201).json(updatedProducts);
       }
-    //   if(req.method === "PUT"){
-    //    const product = getCommentById()
-    //   }
-      return res.status(404).json({message:"NOT FOND"});
+      return res.status(404).json({message:"NOT  FOND"});
   }
