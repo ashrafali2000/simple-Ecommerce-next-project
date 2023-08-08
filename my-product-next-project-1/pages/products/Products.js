e5c1423a7b170495972483bc4a5212ebea4324df
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-08-08 17:30:27 +0500

    card file

--- my-product-next-project-1/pages/products/Products.js
+++ my-product-next-project-1/pages/products/Products.js
@@ -1,15 +1,31 @@
 import { Button, Card } from "antd";
 import Link from "next/link";
-import {BiLike} from "react-icons/bi"
-import {AiOutlineHeart} from "react-icons/ai"
+import { BiLike } from "react-icons/bi"
+import { AiOutlineHeart } from "react-icons/ai"
 import { useState } from "react";
-import {FaRegCommentDots} from "react-icons/fa"
-// import { getCommentById } from "@/services/products";
-function handleInput(event) {
-    let value = event.target.value;
-    setFocusText(value);
- }
-export default function Products({title,img,myKey,stock,price, myImages}) {
+import { FaRegCommentDots } from "react-icons/fa"
+import { useRef } from "react";
+export default function Products({ title, img, myKey, stock, price, myImages }) {
+    const commentRef = useRef();
+
+    const mySubmit = (event) => {
+        const comment = commentRef.current.value;
+        event.preventDefault();
+        console.log(comment)
+        const newProduct = JSON.stringify({ myKey, comment });
+        fetch(`/api/products/`, {
+            method: "POST",
+            body: newProduct,
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+            .then((res) => res.json())
+            .then((json) => console.log(json));
+
+
+        console.log(newProduct)
+    }
 
     const [hideLightbox, setHideLightbox] = useState(true);
 
@@ -28,76 +44,76 @@ export default function Products({title,img,myKey,stock,price, myImages}) {
     return (
         <div className="Products">
             <Card
-             style={{
-                width: 320,
-                height: 480,
-                backgroundColor: "#e3c986",
-                color: "#000",
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                alignItems: "center",
-                boxShadow: "0px 4px 13px #777"
-              }}
-              cover={
-            <img className="cardImg" style={{
-                    height: 260,
-                    width: 250,
+                style={{
+                    width: 320,
+                    height: 480,
+                    backgroundColor: "#e3c986",
+                    color: "#000",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    boxShadow: "0px 4px 13px #777"
                 }}
-                  alt="example"
-                  src={`${img}`}
-                />
-            }
+                cover={
+                    <img className="cardImg" style={{
+                        height: 260,
+                        width: 250,
+                    }}
+                        alt="example"
+                        src={`${img}`}
+                    />
+                }
             >
                 <div
-                style={{
-                    display: "flex",
-                    justifyContent: "space-between",
-                    alignItems: "center",
-                    flexDirection:"column",
-                    gap: 25,
-                    fontSize: 18
-                }}>
+                    style={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                        flexDirection: "column",
+                        gap: 25,
+                        fontSize: 18
+                    }}>
 
-                {title}
+                    {title}
+                    <div style={{
+                        fontSize: 30,
+                        width: 230,
+                        display: "flex",
+                        justifyContent: "space-between",
+
+                    }}>
+                        <div
+                            style={{
+                                fontSize: 20,
+                                width: 60,
+                                display: "flex",
+                                justifyContent: "space-between",
+                                alignItems: "center"
+                            }} ><BiLike style={{ fontSize: 30 }} onClick={likeHandler} />{like}</div>
+                        <div
+                            style={{
+                                fontSize: 20,
+                                width: 70,
+                                display: "flex",
+                                justifyContent: "space-between",
+                                alignItems: "center"
+                            }}> {love} <AiOutlineHeart style={{ fontSize: 30 }} onClick={loveHandler} /></div>
+                        <div style={{
+                            fontSize: 30,
+
+                        }}
+                        >
+                            <form className={`lightbox ${hideLightbox ? "hide-lightbox" : ""}`} onSubmit={mySubmit} ><input autoFocus ref={commentRef}></input> <button className="savebtn">Send</button> </form>
+                            <FaRegCommentDots onClick={() => setHideLightbox(false)}></FaRegCommentDots>
+                        </div>
+                    </div>
+                </div>
+
                 <div style={{
-                    fontSize: 30,
-                    width: 230,
-                    display: "flex",
-                    justifyContent: "space-between",
-
-                }}>
-              <div 
-              style={{
-                fontSize: 20,
-                width: 60,
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems:"center"
-                }} ><BiLike style={{fontSize: 30}} onClick={likeHandler} />{like}</div>  
-               <div
-               style={{
-                fontSize: 20,
-                width: 70,
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems:"center"
-            }}> {love} <AiOutlineHeart style={{fontSize: 30}}  onClick={loveHandler}/></div>
-            <div style={{
-                fontSize:30,
-
-            }}
-            >
-               <form className={`lightbox ${hideLightbox ? "hide-lightbox" : ""}`}><input autoFocus></input> <button className="savebtn">Send</button> </form> 
-           <FaRegCommentDots onClick={() => setHideLightbox(false) }></FaRegCommentDots>
-            </div>
-                </div>
-                </div>
-
-             <div style={{
-                textAlign: "start",
-                marginTop: 15
-             }}><Link href={`api/products/${myKey}`}><button className="seeDetailsBtn">See Details</button></Link></div>   
+                    textAlign: "start",
+                    marginTop: 15
+                }}><Link href={`productDetails/${myKey}`}><button className="seeDetailsBtn">See Details</button></Link></div>
             </Card>
         </div>
     )
