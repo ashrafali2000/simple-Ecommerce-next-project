874a77ebc569602cef791cced9f39cf319ef99db
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-08-09 17:30:46 +0500

    products creating

--- my-product-next-project-1/pages/api/products/index.js
+++ my-product-next-project-1/pages/api/products/index.js
@@ -1,20 +1,30 @@
-import { getAllProducs, addProduct,getCommentById } from "@/services/products"
+import { getAllProducs, addProduct,getCommentById, getAllAccounts } from "@/services/products"
 
 export default function handler(req, res) {
+    // render all products as a cards
     if(req.method === "GET") {
         const product = getAllProducs();
         res.status(200).json(product);
     }
-    if (req.method === "POST") {
-        const {title,description,price, discountPercentage, rating,stock,brand,category,images } = req.body;
-        const updatedProducts = addProduct(title,description,price, discountPercentage, rating,stock,brand,category,images);
-        return res.status(201).json(updatedProducts);
-      }
-    if (req.method === "POST") {
-        const {myKey,comment } = req.body;
-        const updatedProducts = getCommentById(myKey,comment);
-        // console.log(id, comment);
-        return res.status(201).json(updatedProducts);
-      }
-      return res.status(404).json({message:"NOT  FOND"});
+
+
+    // creating products
+    // if (req.method === "POST") {
+    //     const {title,description,price, discountPercentage, rating,stock,brand,category,images } = req.body;
+    //     const updatedProducts = addProduct(title,description,price, discountPercentage, rating,stock,brand,category,images);
+    //     return res.status(201).json(updatedProducts);
+    //   }
+
+
+    // creating comment
+    // if (req.method === "POST") {
+    //     const {myKey,comment } = req.body;
+    //     const updatedProducts = getCommentById(myKey,comment);
+    //     // console.log(id, comment);
+    //     return res.status(201).json(updatedProducts);
+    //   }
+      
+        return res.status(404).json({message:"NOT  FOND"});
   }
+
+
