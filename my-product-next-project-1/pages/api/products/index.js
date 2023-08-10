e40d8511087ade982c6d00cba68a3064aaffd7a0
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-08-10 17:30:01 +0500

    open the comments

--- my-product-next-project-1/pages/api/products/index.js
+++ my-product-next-project-1/pages/api/products/index.js
@@ -9,11 +9,11 @@ export default function handler(req, res) {
 
 
     // creating products
-    // if (req.method === "POST") {
-    //     const {title,description,price, discountPercentage, rating,stock,brand,category,images } = req.body;
-    //     const updatedProducts = addProduct(title,description,price, discountPercentage, rating,stock,brand,category,images);
-    //     return res.status(201).json(updatedProducts);
-    //   }
+    if (req.method === "POST") {
+        const {title,description,price, discountPercentage, rating,stock,brand,category,images } = req.body;
+        const updatedProducts = addProduct(title,description,price, discountPercentage, rating,stock,brand,category,images);
+        return res.status(201).json(updatedProducts);
+      }
 
 
     // creating comment
