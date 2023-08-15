3be7161cb8aec68ff4713b80c99a2c49ce628ddb
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-08-15 15:45:10 +0500

    Pricing , feature, News letter section

--- my-product-next-project-1/pages/about/index.js
+++ my-product-next-project-1/pages/about/index.js
@@ -10,9 +10,34 @@ const links = [
     { name: 'Hours per week', value: '40' },
     { name: 'Paid time off', value: 'Unlimited' },
   ]
-  
-  export default function Example() {
+  const features = [
+    { name: 'Origin', description: 'Designed by Good Goods, Inc.' },
+    { name: 'Material', description: 'Solid walnut base with rare earth magnets and powder coated steel card cover' },
+    { name: 'Dimensions', description: '6.25" x 3.55" x 1.15"' },
+    { name: 'Finish', description: 'Hand sanded and finished with natural oil' },
+    { name: 'Includes', description: 'Wood card tray and 3 refill packs' },
+    { name: 'Considerations', description: 'Made from natural materials. Grain and color vary with each item.' },
+  ]
+  import { CheckIcon } from '@heroicons/react/20/solid'
+  import { CalendarDaysIcon, HandRaisedIcon } from '@heroicons/react/24/outline'
+  const stats1 = [
+    { id: 1, name: 'Transactions every 24 hours', value: '44 million' },
+    { id: 2, name: 'Assets under holding', value: '$119 trillion' },
+    { id: 3, name: 'New users annually', value: '46,000' },
+  ]
+const includedFeatures = [
+  'Private forum access',
+  'Member resources',
+  'Entry to annual conference',
+  'Official member t-shirt',
+]
+
+
+  export default function About() {
     return (
+      <div>
+
+        {/* Header Section */}
       <div className="relative isolate overflow-hidden bg-gray-500 py-24 sm:py-32">
         <img
           src="https://t4.ftcdn.net/jpg/05/64/60/45/360_F_564604544_KNKxKUoQdEu8yYSQlrygkzdKRnuR1zSp.jpg"
@@ -69,6 +94,192 @@ const links = [
             </dl>
           </div>
         </div>
+      </div>
+
+      {/* Product Features Section */}
+      <div className="bg-white">
+      <div className="mx-auto grid max-w-2xl grid-cols-1 items-center gap-x-8 gap-y-16 px-4 py-24 sm:px-6 sm:py-32 lg:max-w-7xl lg:grid-cols-2 lg:px-8">
+        <div>
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Our Products Features:</h2>
+          <p className="mt-4 text-gray-500">
+            The iphone is the world familer mobile in the range and the quality, So every one like this mobile phone this prices of these mobile phone from 10 lac to 20 lac of the heighest insurance and smallest quality is 90 thousands in the world so come to our website and purchase for all of your family and your.
+          </p>
+
+          <dl className="mt-16 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8">
+            {features.map((feature) => (
+              <div key={feature.name} className="border-t border-gray-200 pt-4">
+                <dt className="font-medium text-gray-900">{feature.name}</dt>
+                <dd className="mt-2 text-sm text-gray-500">{feature.description}</dd>
+              </div>
+            ))}
+          </dl>
+        </div>
+        <div className="grid grid-cols-2 grid-rows-2 gap-4 sm:gap-6 lg:gap-8">
+          <img
+            src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-card-40-iphone14-202209_FMT_WHH?wid=508&hei=472&fmt=p-jpg&qlt=95&.v=1661958160494"
+            alt="Walnut card tray with white powder coated steel divider and 3 punchout holes."
+            className="rounded-lg bg-gray-100"
+          />
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwRxyXJbNYkHAiVG_Q_AcVDDAkF8Vu26PUyQ&usqp=CAU"
+            alt="Top down view of walnut card tray with embedded magnets and card groove."
+            className="rounded-lg bg-gray-100"
+          />
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnDjSrWplXC4NZQqPR-fPvynCob-vZ0u4A7g&usqp=CAU"
+            alt="Side of walnut card tray with card groove and recessed card area."
+            className="rounded-lg bg-gray-100"
+          />
+          <img
+            src="https://www.digitaltrends.com/wp-content/uploads/2022/09/apple-iphone-14-review-11.jpg?resize=625%2C417&p=1"
+            alt="Walnut card tray filled with cards and card angled in dedicated groove."
+            className="rounded-lg bg-gray-100"
+          />
+        </div>
+      </div>
+    </div>
+        
+
+{/* Pricing Section */}
+    <div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl sm:text-center">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Simple no-tricks pricing</h2>
+          <p className="mt-6 text-lg leading-8 text-gray-600">
+            Distinctio et nulla eum soluta et neque labore quibusdam. Saepe et quasi iusto modi velit ut non voluptas
+            in. Explicabo id ut laborum.
+          </p>
+        </div>
+        <div className="mx-auto mt-16 max-w-2xl rounded-3xl ring-1 ring-gray-200 sm:mt-20 lg:mx-0 lg:flex lg:max-w-none">
+          <div className="p-8 sm:p-10 lg:flex-auto">
+            <h3 className="text-2xl font-bold tracking-tight text-gray-900">Lifetime membership</h3>
+            <p className="mt-6 text-base leading-7 text-gray-600">
+              Lorem ipsum dolor sit amet consect etur adipisicing elit. Itaque amet indis perferendis blanditiis
+              repellendus etur quidem assumenda.
+            </p>
+            <div className="mt-10 flex items-center gap-x-4">
+              <h4 className="flex-none text-sm font-semibold leading-6 text-indigo-600">What’s included</h4>
+              <div className="h-px flex-auto bg-gray-100" />
+            </div>
+            <ul
+              role="list"
+              className="mt-8 grid grid-cols-1 gap-4 text-sm leading-6 text-gray-600 sm:grid-cols-2 sm:gap-6"
+            >
+              {includedFeatures.map((feature) => (
+                <li key={feature} className="flex gap-x-3">
+                  <CheckIcon className="h-6 w-5 flex-none text-indigo-600" aria-hidden="true" />
+                  {feature}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="-mt-2 p-2 lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0">
+            <div className="rounded-2xl bg-gray-50 py-10 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center lg:py-16">
+              <div className="mx-auto max-w-xs px-8">
+                <p className="text-base font-semibold text-gray-600">Pay once, own it forever</p>
+                <p className="mt-6 flex items-baseline justify-center gap-x-2">
+                  <span className="text-5xl font-bold tracking-tight text-gray-900">$349</span>
+                  <span className="text-sm font-semibold leading-6 tracking-wide text-gray-600">USD</span>
+                </p>
+                <a
+                  href="#"
+                  className="mt-10 block w-full rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Get access
+                </a>
+                <p className="mt-6 text-xs leading-5 text-gray-600">
+                  Invoices and receipts available for easy company reimbursement
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+{/* News Letter Section */}
+<div className="relative isolate overflow-hidden bg-gray-800 py-16 sm:py-24 lg:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-2">
+          <div className="max-w-xl lg:max-w-lg">
+            <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">Subscribe to our newsletter.</h2>
+            <p className="mt-4 text-lg leading-8 text-gray-300">
+              Nostrud amet eu ullamco nisi aute in ad minim nostrud adipisicing velit quis. Duis tempor incididunt
+              dolore.
+            </p>
+            <div className="mt-6 flex max-w-md gap-x-4">
+              <label htmlFor="email-address" className="sr-only">
+                Email address
+              </label>
+              <input
+                id="email-address"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                className="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                placeholder="Enter your email"
+              />
+              <button
+                type="submit"
+                className="flex-none rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+              >
+                Subscribe
+              </button>
+            </div>
+          </div>
+          <dl className="grid grid-cols-1 gap-x-8 gap-y-10 sm:grid-cols-2 lg:pt-2">
+            <div className="flex flex-col items-start">
+              <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
+                <CalendarDaysIcon className="h-6 w-6 text-white" aria-hidden="true" />
+              </div>
+              <dt className="mt-4 font-semibold text-white">Weekly articles</dt>
+              <dd className="mt-2 leading-7 text-gray-400">
+                Non laboris consequat cupidatat laborum magna. Eiusmod non irure cupidatat duis commodo amet.
+              </dd>
+            </div>
+            <div className="flex flex-col items-start">
+              <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
+                <HandRaisedIcon className="h-6 w-6 text-white" aria-hidden="true" />
+              </div>
+              <dt className="mt-4 font-semibold text-white">No spam</dt>
+              <dd className="mt-2 leading-7 text-gray-400">
+                Officia excepteur ullamco ut sint duis proident non adipisicing. Voluptate incididunt anim.
+              </dd>
+            </div>
+          </dl>
+        </div>
+      </div>
+      <div className="absolute left-1/2 top-0 -z-10 -translate-x-1/2 blur-3xl xl:-top-6" aria-hidden="true">
+        <div
+          className="aspect-[1155/678] w-[72.1875rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
+          style={{
+            clipPath:
+              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+          }}
+        />
+      </div>
+    </div>
+
+{/* Stats Section */}
+
+<div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <dl className="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-3">
+          {stats1.map((stat) => (
+            <div key={stat.id} className="mx-auto flex max-w-xs flex-col gap-y-4">
+              <dt className="text-base leading-7 text-gray-600">{stat.name}</dt>
+              <dd className="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
+                {stat.value}
+              </dd>
+            </div>
+          ))}
+        </dl>
+      </div>
+    </div>
+
+
+
       </div>
     )
   }
