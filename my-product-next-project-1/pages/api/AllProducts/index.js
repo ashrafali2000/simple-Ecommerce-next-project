847a877b3aa88a8b8d19f4736f9487af51a450a1
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-08-16 10:20:59 +0500

    create post method for products

--- my-product-next-project-1/pages/api/AllProducts/index.js
+++ my-product-next-project-1/pages/api/AllProducts/index.js
@@ -1,4 +1,4 @@
-import { getAllProducs1 } from "@/services/products"
+import { getAllProducs1,getCommentById1 } from "@/services/products"
 
 export default function handler(req, res) {
     // render all products as a cards
@@ -6,4 +6,13 @@ export default function handler(req, res) {
         const product = getAllProducs1();
         res.status(200).json(product);
     }
+    if (req.method === "POST") {
+        const {myKey,comment } = req.body;
+        const updatedComments = getCommentById1(myKey,comment);
+        // console.log(id, comment);
+        return res.status(201).json(updatedComments);
+      }
+      return res.status(404).json({message:"NOT  FOND"});
 }
+
+  
