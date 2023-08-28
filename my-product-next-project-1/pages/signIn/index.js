c2e221bca81c32f85affcca3b526538748f8696d
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-08-28 16:04:00 +0500

    set logo in signin and signOut

--- my-product-next-project-1/pages/signIn/index.js
+++ my-product-next-project-1/pages/signIn/index.js
@@ -1,5 +1,7 @@
 import { useRef, useState } from "react"
 import Link from "next/link";
+import myLogo from "../../public/LogoProducts.png"
+import Image from "next/image";
 
 export default function SingnIn({setHideProducts,setUserName,setUserImg,setIsLogin, setLogo}) {
 
@@ -49,9 +51,9 @@ export default function SingnIn({setHideProducts,setUserName,setUserImg,setIsLog
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
-            className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+          <Image
+       className="mx-auto h-12 w-auto"
+            src={myLogo}
             alt="Your Company"
           />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
--- my-product-next-project-1/pages/signUp/index.js
+++ my-product-next-project-1/pages/signUp/index.js
@@ -1,6 +1,7 @@
 import { useRef,useState } from "react"
 import Link from "next/link";
-
+import myLogo from "../../public/LogoProducts.png"
+import Image from "next/image";
 export default function SignUp() {
   const nameRef =  useRef();
   const emailRef = useRef();
@@ -59,9 +60,9 @@ setCheck(false);
       <>
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <img
+            <Image
               className="mx-auto h-10 w-auto"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+              src={myLogo}
               alt="Your Company"
             />
             <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
