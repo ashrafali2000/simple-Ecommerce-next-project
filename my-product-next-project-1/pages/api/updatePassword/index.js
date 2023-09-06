43750cce482fc9e3c76bae2d4f012f8d17ee76d2
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-09-06 16:48:05 +0500

    just await keyword add

--- my-product-next-project-1/pages/api/updatePassword/index.js
+++ my-product-next-project-1/pages/api/updatePassword/index.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
         try{
           const myUser = getByEmail(userEmail);
           const myUserPassword = myUser.password;
-          const userVerifyPassword = verifyUserPassword(oldPassword, myUserPassword, userEmail,newPassword);
+          const userVerifyPassword = await verifyUserPassword(oldPassword, myUserPassword, userEmail,newPassword);
           console.log(typeof(oldPassword))
           res.status(200).json(userVerifyPassword);
         }catch(err){
